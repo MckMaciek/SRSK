f573f53d0a0e526673a630a0aee4add9514ff65d
Maciej <mckmusial@gmail.com>
2020-10-25 00:56:40 +0200

    Register and Login Screen

--- src/main/java/com/example/SRSK/controllers/IndexControler.java
+++ src/main/java/com/example/SRSK/controllers/IndexControler.java
@@ -7,8 +7,8 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class IndexControler {
 
-    @RequestMapping("/")
+    @RequestMapping("/login")
     public ModelAndView index() {
-        return new ModelAndView("index.html");
+        return new ModelAndView("login.html");
     }
 }
